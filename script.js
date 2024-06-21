7862a39037ad05a4e34cd074022e99487a27d70e
Emiliano G <ultralightened@gmail.com>
2024-06-20 20:00:10 -0400

    Add functionality to reset button
    Prompts asking the user for squares per side
    desired, removes previous grid and inserts new one

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ const resetButton = document.createElement("button")
 resetButton.textContent = "Create new grid";
 resetButton.style.padding = "6px";
 resetButton.style.margin = "32px auto";
+resetButton.style.fontSize = "24px";
 body.insertBefore(resetButton, container);
 
-
 insertDivsToCanvas();
+resetButton.addEventListener("click", generateNewGrid);
 
+
+
+
+
+
+// ###########################################
 
 function insertDivsToCanvas(squaresPerSide = 16) {
-
     let numberSquared = squaresPerSide * squaresPerSide;
     for (i = 0; i < numberSquared; i++) {
         createDivs(squaresPerSide);
     }
-    console.log(i);
-    console.log(squaresPerSide);
 }
 
 function createDivs(squaresPerSide) {
@@ -29,5 +33,10 @@ function createDivs(squaresPerSide) {
     div.style = `width: ${squareSize}px; height: ${squareSize}px`
     container.appendChild(div);
     div.addEventListener("mouseover", () => div.style.backgroundColor = "gray");
+}
 
+function generateNewGrid(input) {
+    input = prompt("How many squares per side would you want?");
+    container.textContent = "";
+    insertDivsToCanvas(input);
 }
